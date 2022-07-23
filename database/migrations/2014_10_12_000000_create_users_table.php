22c8959978f1ecd82fd73750537efdc818732210
Rivaldi Palermo <rivaldi.palermo11@gmail.com>
2022-07-23 11:11:36 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('jenis_kelamin');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
