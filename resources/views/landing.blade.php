d5a4fdd4c6c1cb15f077230afde4105a32fa499d
JuliaditS <juliaditsyahputra@gmail.com>
2022-07-23 12:10:58 +0700

    profile2

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -15,7 +15,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="/profile"><img src=<?=url("download2.png")?> height="40" width="40"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -27,7 +27,7 @@
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="/profile">Profile</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,10 +1,23 @@
 @extends('landing')
 
 @section('content')
-<div>
-Profil Singkat
-
-Papua adalah sebuah provinsi di Indonesia yang terletak di pulau Nugini bagian barat atau west New Guinea. Papua juga sering disebut sebagai Papua Barat karena Papua bisa merujuk kepada seluruh pulau Nugini termasuk belahan timur negara tetangga, east New Guinea atau Papua Nugini. Papua Barat adalah sebutan yang lebih disukai para nasionalis yang ingin memisahkan diri dari Indonesia dan membentuk negara sendiri. Provinsi ini dulu dikenal dengan panggilan Irian Barat sejak tahun 1969 hingga 1973, namanya kemudian diganti menjadi Irian Jaya oleh Soeharto pada saat meresmikan tambang tembaga dan emas Freeport, nama yang tetap digunakan secara resmi hingga tahun 2002. Nama provinsi ini diganti menjadi Papua sesuai UU No 21/2001 Otonomi Khusus Papua. Pada masa era kolonial Belanda, daerah ini disebut Nugini Belanda (Dutch New Guinea).
-Provinsi Papua, sebelumnya mencakup seluruh wilayah Indonesia di Pulau Papua. Pada masa pemerintahan kolonial Hindia Belanda, wilayah ini dikenal sebagai Nugini Belanda (Nederlands Nieuw-Guinea atau Dutch New Guinea). Setelah berada bergabung dengan Negara Kesatuan Republik Indonesia, wilayah ini dikenal sebagai Provinsi Irian Barat sejak tahun 1963 hingga 1973. Namanya kemudian diganti menjadi Irian Jaya oleh Soeharto pada saat meresmikan tambang tembaga dan emas Freeport, nama yang tetap digunakan secara resmi sampai terbitnya Undang-Undang No. 21 Tahun 2001 tentang Otonomi Khusus Papua mengamanatkan nama provinsi ini untuk diganti menjadi Papua.
+<div class="row">
+    <div class=col-sm-8>
+        <div class="card" style="width: 50rem;">
+            <div class="card-body">
+                <h5 class="card-title">Provil Singkat</h5>
+                <p class="card-text">Papua adalah sebuah provinsi di Indonesia yang terletak di pulau Nugini bagian barat atau west New Guinea. Papua juga sering disebut sebagai Papua Barat karena Papua bisa merujuk kepada seluruh pulau Nugini termasuk belahan timur negara tetangga, east New Guinea atau Papua Nugini. Papua Barat adalah sebutan yang lebih disukai para nasionalis yang ingin memisahkan diri dari Indonesia dan membentuk negara sendiri. Provinsi ini dulu dikenal dengan panggilan Irian Barat sejak tahun 1969 hingga 1973, namanya kemudian diganti menjadi Irian Jaya oleh Soeharto pada saat meresmikan tambang tembaga dan emas Freeport, nama yang tetap digunakan secara resmi hingga tahun 2002. Nama provinsi ini diganti menjadi Papua sesuai UU No 21/2001 Otonomi Khusus Papua. Pada masa era kolonial Belanda, daerah ini disebut Nugini Belanda (Dutch New Guinea). Provinsi Papua, sebelumnya mencakup seluruh wilayah Indonesia di Pulau Papua. Pada masa pemerintahan kolonial Hindia Belanda, wilayah ini dikenal sebagai Nugini Belanda (Nederlands Nieuw-Guinea atau Dutch New Guinea). Setelah berada bergabung dengan Negara Kesatuan Republik Indonesia, wilayah ini dikenal sebagai Provinsi Irian Barat sejak tahun 1963 hingga 1973. Namanya kemudian diganti menjadi Irian Jaya oleh Soeharto pada saat meresmikan tambang tembaga dan emas Freeport, nama yang tetap digunakan secara resmi sampai terbitnya Undang-Undang No. 21 Tahun 2001 tentang Otonomi Khusus Papua mengamanatkan nama provinsi ini untuk diganti menjadi Papua.</p>
+            </div>
+        </div>
+    </div>
+    <div class=col-sm-4>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title"><img src=<?=url("download2.png")?> height="200" width="200"></h5>
+            </div>
+        </div>
+    </div>
 </div>
+
+
 @endsection
